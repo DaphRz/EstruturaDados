a93549639db51c59a3ddc9c52c74325e2ed21f6b
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-09-03 10:49:21 -0300

    segunda aula - insertion sort

--- 6. Ordenação.c
+++ 6. Ordenação.c
@@ -44,3 +44,97 @@
   }
 
 // Bubble Sort
+
+  void bubbleSort (int *v, int tam) {
+    
+  int troca, aux;
+  
+  do {
+      
+      troca = 0;
+      
+      for(int i = 0; i < tam-1; i++){
+          
+          if(v[i]>v[i+1]){
+              
+            aux = v[i];
+            v[i] = v[i+1];
+            v[i+1] = aux;
+            
+            troca = 1;
+          }
+      }
+      
+    } while(troca);  // troca == 1  == true 
+                  // if (troca == false) sai do loop
+  }
+
+  void imprimir(int *v, int n){
+    
+    printf("\nVetor: ");
+    
+    for(int i = 0; i<n; i++){
+        printf(" %d ", v[i]);
+      }
+  }
+
+  int main(){
+    
+    int vet [] = {-8,90,23,23,54,7,45};
+
+    imprimir(vet,7);
+    printf("\n");
+
+    bubbleSort(vet,7);
+    
+    imprimir(vet,7);
+
+    return 0;
+  }
+
+// Insertion Sort
+
+  void insertionSort (int *v, int tam) {
+    
+    int aux,aux1;
+    aux1 = 1;
+  
+    for(int i = 0; i < tam; i++){
+    
+        if(v[i+1]<v[i]){
+          
+            aux = v[i+1];
+            v[i] = v[i+1];
+            v[i] = aux;
+        }
+        
+    aux1++;
+    
+                            //for(int n = tam; n = 0; n--){
+                                
+                            //}
+    }
+  }
+
+  void imprimir(int *v, int n){
+    
+    printf("\nVetor: ");
+    
+    for(int i = 0; i<n; i++){
+        printf(" %d ", v[i]);
+    }
+  }
+
+  int main(){
+    
+    int vet [] = {-8,90,23,23,54,7,45};
+
+    imprimir(vet,7);
+    printf("\n");
+
+    insertionSort(vet,7);
+    
+    imprimir(vet,7);
+
+    return 0;
+  }
