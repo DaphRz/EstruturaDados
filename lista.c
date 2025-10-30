d65f1eb9c55849803c5f7daead87dbd75adbfb81
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-10-30 20:12:06 -0300

    ok

--- lista.c
+++ lista.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 typedef struct cel
 {
   int conteudo;
   struct cel *seg;
-}cel;
+} cel;
 
 typedef struct cel* Lista;
 
@@ -18,16 +18,22 @@ Lista* cria_lista(){
 }
 
 int insere_lista_fim(Lista* lista, int x){
+  
   if(lista==NULL) {return 0;}
+  
   cel* aux = (cel*) malloc(sizeof(cel));
+  
   if(aux==NULL){return 0;}
+  
   aux->conteudo = x;
   aux->seg = NULL;
-  if((*lista)==NULL){ *lista = aux;}
+  
+  if(*lista == NULL){*lista = aux;}
   else{
     cel *temp;
     temp = *lista;
-    while(temp->seg!= NULL){ //caminha até o utlimo elemento
+    
+    while(temp->seg!= NULL){   //caminha até o utlimo elemento
       temp= temp->seg;
     }
     temp->seg = aux;
@@ -36,11 +42,15 @@ int insere_lista_fim(Lista* lista, int x){
 }
 
 int imprime_lista(Lista* lista){
+  
     if(lista==NULL) {
       printf("Lista Não Existe");
     }
+  
     cel *temp = *lista;
+  
     printf("\n Minha lista:");
+  
     while(temp != NULL){ 
         printf("\t%d", temp->conteudo);
         temp= temp->seg;
@@ -50,6 +60,7 @@ int imprime_lista(Lista* lista){
 }
 
 int busca_lista(Lista* lista, int x){
+  
     if(lista==NULL) {
       printf("Lista Não Existe");
       return 0;
@@ -66,30 +77,31 @@ int busca_lista(Lista* lista, int x){
     return 0;
 }
 
-void libera_lista(Lista *lista)
+void libera_lista(Lista *lista)  // liberar toda a lista da memória — nó por nó, e depois o próprio ponteiro da lista.
 {
     if(lista != NULL)
     {
         cel *aux;
         
-        while(*lista != NULL)
+        while(*lista != NULL)  // (?)  
         {
             aux = *lista;
             
             *lista = (*lista) -> seg;
             free(aux);
         }
+      
         free(lista);
     }    
 }
 
-int remove_lista(Lista *lista,int x)
+int remove_lista(Lista *lista,int x)   // Remover apenas um elemento específico (x) da lista. 
 {
-    if(lista == NULL) {return 0;} //lista existe?
-    if(*lista == NULL) {return 0;} // lista esta vazia?
+    if(lista == NULL) {return 0;}   // lista existe?
+    if(*lista == NULL) {return 0;}  // lista esta vazia?
 
-    cel* anterior, *aux = *lista;
-    
+    cel* anterior, // cel* é um ponteiro de celula - um ponteiro q aponta pra estrutura cel 
+    *aux = *lista;
     
     while(aux != NULL && aux->conteudo != x)
     {
@@ -97,9 +109,9 @@ int remove_lista(Lista *lista,int x)
         aux = aux->seg;
     }
     
-    if(aux == NULL){return 0 ;} //se nao tiver elemento na lista
+    if(aux == NULL){return 0 ;}   // se nao tiver elemento na lista
     
-    if(aux == *lista)     //se for o primeiro elemento da lista
+    if(aux == *lista)     // se for o primeiro elemento da lista
     {
         *lista = aux->seg;
     }
@@ -111,8 +123,6 @@ int remove_lista(Lista *lista,int x)
     return 1;
 }
 
-
-
 int main(void) 
 {
   Lista *lst;
@@ -123,6 +133,7 @@ int main(void)
   insere_lista_fim(lst, 4);
   insere_lista_fim(lst, 5);
   imprime_lista(lst);
+  
   //printf("\n %d",busca_lista(lst, 3));
 
 remove_lista(lst,3);
